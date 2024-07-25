e8617250fcfd1e7abacfbc2a6a8b02f13ecfe749
Xiahui Zhang <xhz0729@gmail.com>
2024-07-25 17:09:09 -0500

    adding a new list element to my ingredient list

---
+++ assignments/recipe/script.js
@@ -0,0 +1,4 @@
+// adding a new ingredient
+let newListEle = document.createElement('li');
+newListEle.innerText = 'spoon for stirring'
+document.getElementById('ingredients').appendChild(newListEle);
