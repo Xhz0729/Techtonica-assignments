08680e584e5e45ff40d18796bdb90a9f3c0531c3
Xiahui Zhang <xhz0729@gmail.com>
2024-07-25 17:24:09 -0500

    chaning color and font size of all h2 elements

--- assignments/recipe/script.js
+++ assignments/recipe/script.js
@@ -2,3 +2,10 @@
 let newListEle = document.createElement('li');
 newListEle.innerText = 'spoon for stirring'
 document.getElementById('ingredients').appendChild(newListEle);
+
+// change the color and font of all h2 elements
+let h2EleList = document.querySelectorAll('h2');
+h2EleList.forEach(function(h2) {
+    h2.style.color = '#003366'; // Change the text color to dark blue
+    h2.style.fontSize = '1.6rem'; // Change the font size (optional)
+  });
