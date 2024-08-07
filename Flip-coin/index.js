9f2bd1bf064a01b432c3a8f19f56e9fef3bff0be
Xiahui Zhang <xhz0729@gmail.com>
2024-08-07 16:08:09 -0500

    active my flip button and my reset button

--- Flip-coin/index.js
+++ Flip-coin/index.js
@@ -1,18 +1,42 @@
 let coin = document.querySelector('#coin');
 let playBtn = document.querySelector('#playBtn');
 let resetBtn = document.querySelector('#resetBtn');
+let headsCnt = document.getElementById('headsCount');
+let tailsCnt = document.getElementById('tailsCount')
 
+let heads = 0;
+let tails = 0;
 playBtn.addEventListener('click', () => {
     let i = Math.floor(Math.random() * 2);
+    coin.style.animation = 'none';
     if (i) {
         setTimeout(function(){
             coin.style.animation = 'heads-spin 3s forwards'
         }, 100);
+        heads++;
     }
+
     else {
         setTimeout(function(){
-            coin.style.animation = 'tails-spin 4s forwards';
-
+            coin.style.animation = 'tails-spin 3s forwards';
         }, 100);
+        tails++;
     }
-})
+    setTimeout(updateStates, 3000);
+});
+
+function updateStates() {
+    document.querySelector('#headsCount').textContent = `Heads: ${heads}`;
+    document.querySelector('#tailsCount').textContent = `Tails: ${tails}`;
+}
+
+// active the reset btn
+resetBtn.addEventListener("click", () => {
+    coin.style.animation = 'none';
+    heads = 0;
+    tails = 0;
+    updateStates();
+});
+
+
+
