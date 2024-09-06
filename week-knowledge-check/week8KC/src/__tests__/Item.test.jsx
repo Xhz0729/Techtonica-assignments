34923da0eeae967a523d601ded9b41df9a9328b7
Xiahui Zhang <xhz0729@gmail.com>
2024-09-06 15:50:59 -0500

    Test if items in the list (Item component) are rendered correctly.

---
+++ week-knowledge-check/week8KC/src/__tests__/Item.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { describe, expect, test } from "vitest";
+import Item from "../components/item";
+
+describe("Item component rendering", () => {
+  test("renders Item component properly when text is provided", () => {
+    // Render the Item component with a non-empty text prop
+    render(<Item item={{ text: "Learn Testing" }} />);
+
+    // Check if the item text is rendered
+    const itemEle = screen.getByText(/Learn Testing/i);
+    expect(itemEle).toBeInTheDocument();
+  });
+});
