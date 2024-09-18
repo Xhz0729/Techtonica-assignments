b6a9a16d108c415fec923e215c97696daf7a144b
Xiahui Zhang <xhz0729@gmail.com>
2024-09-17 21:48:26 -0500

    assigning endpoints for different routes

--- animal-sighting-tracker/server/routes/animals.js
+++ animal-sighting-tracker/server/routes/animals.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getSpecies } from "../controller/animals.js";
+import {
+  getSpecies,
+  getSightings,
+  addSighting,
+} from "../controller/animals.js";
 
 const router = express.Router();
 // get all species
 router.get("/species", getSpecies);
+
+// get sightings
+router.get("/sightings", getSightings);
+
+// add a sighting
+router.post("/sightings", addSighting);
 
 export default router;
