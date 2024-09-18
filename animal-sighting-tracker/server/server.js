b38790048f64b953971e79c46683bcfde84a6df4
Xiahui Zhang <xhz0729@gmail.com>
2024-09-17 21:49:27 -0500

    assigning /animals as the endpoint for animalRoutes

--- animal-sighting-tracker/server/server.js
+++ animal-sighting-tracker/server/server.js
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors";
 import animalsRoutes from "./routes/animals.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
+app.use(express.json());
+
 // creates an endpoint for the route "/""
 app.get("/", (req, res) => {
-  res.json({ message: "Hola, from My template ExpressJS with React-Vite" });
+  res.json({ message: "Hola from the backend server" });
 });
 
 app.use("/animals", animalsRoutes);
