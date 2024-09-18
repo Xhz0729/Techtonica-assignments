a8d5da8f78bfc7ec115c0bda8540e874fa421bf3
Xiahui Zhang <xhz0729@gmail.com>
2024-09-17 21:47:31 -0500

    adding the logic to get all sightings with individual nicknames and the logic to to add a new sighting record

--- animal-sighting-tracker/server/controller/animals.js
+++ animal-sighting-tracker/server/controller/animals.js
@@ -9,3 +9,48 @@ export const getSpecies = async (req, res) => {
     return res.status(400).json({ e });
   }
 };
+
+// route to get all sightings with individual nicknames
+export const getSightings = async (req, res) => {
+  try {
+    const query = `
+      SELECT sightings.*, individuals.nickname
+      FROM sightings
+      JOIN individuals ON sightings.individual_id = individuals.id
+      ORDER BY sightings.sighting_date DESC;
+    `;
+    const { rows: sightings } = await db.query(query);
+    res.json(sightings); // Send the fetched data as a JSON response
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+};
+
+// route to add a new sighting record
+export const addSighting = async (req, res) => {
+  const { sighting_date, individual_id, location, is_healthy, sighter_email } =
+    req.body;
+  try {
+    // check whether the individual exist
+    const individualCheck = await db.query(
+      "SELECT * FROM individuals WHERE id = $1",
+      [individual_id]
+    );
+    if (individualCheck.rows.length === 0) {
+      return res
+        .status(400)
+        .json({ error: "Individual with this ID does not exist" });
+    }
+    // insert the new sighting
+    const newSighting = await db.query(
+      `INSERT INTO sightings (sighting_date, individual_id, location, is_healthy, sighter_email)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *`,
+      [sighting_date, individual_id, location, is_healthy, sighter_email]
+    );
+    res.json(newSighting.rows[0]);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ e });
+  }
+};
