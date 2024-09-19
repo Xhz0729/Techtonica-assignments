ce251a01907993123e52678281450da4ef1a935f
Xiahui Zhang <xhz0729@gmail.com>
2024-09-19 14:10:36 -0500

    rendering search sightings component

--- animal-sighting-tracker/client/src/App.jsx
+++ animal-sighting-tracker/client/src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 import { useReducer, useEffect } from 'react';
 import SightingList from './components/SightingList.jsx';
 import AddSightingForm from './components/AddSightingForm.jsx';
+import SearchSightings from './components/SearchSightings.jsx';
 
 const ACTIONS = {
   SET_SIGHTINGS: "set_sightings",
 
-  ADD_SIGHTING: "add_sighting"
+  ADD_SIGHTING: "add_sighting",
+
+  SEARCH_SIGHTINGS:"search_sightings"
+  
 }
 
 const animalsReducer = (state, action) => {
@@ -15,6 +19,8 @@ const animalsReducer = (state, action) => {
       return action.payload;
     case ACTIONS.ADD_SIGHTING:
       return [...state, action.payload];
+    case ACTIONS.SEARCH_SIGHTINGS:
+      return action.payload;
     default:
       return state;
   }
@@ -45,6 +51,7 @@ function App() {
         <h1>Show sightings</h1>
         <AddSightingForm dispatch={dispatch} />
         <SightingList sightings={sightings} dispatch={dispatch} />
+        <SearchSightings dispatch={dispatch} />
       </div>
     </>
   )
