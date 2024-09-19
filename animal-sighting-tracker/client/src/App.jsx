4211a8e184065f103f8f82a5270227a44a5dc4d3
Xiahui Zhang <xhz0729@gmail.com>
2024-09-18 22:02:06 -0500

    rendering add sighting form on page

--- animal-sighting-tracker/client/src/App.jsx
+++ animal-sighting-tracker/client/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import { useReducer, useEffect } from 'react';
 import SightingList from './components/SightingList.jsx';
+import AddSightingForm from './components/AddSightingForm.jsx';
 
 const ACTIONS = {
-  SET_SIGHTINGS: "set_sightings"
+  SET_SIGHTINGS: "set_sightings",
+
+  ADD_SIGHTING: "add_sighting"
 }
 
 const animalsReducer = (state, action) => {
   switch(action.type) {
     case ACTIONS.SET_SIGHTINGS:
       return action.payload;
+    case ACTIONS.ADD_SIGHTING:
+      return [...state, action.payload];
     default:
       return state;
   }
@@ -31,6 +36,7 @@ function App() {
     <>
       <div>
         <h1>Show sightings</h1>
+        <AddSightingForm dispatch={dispatch} />
         <SightingList sightings={sightings} dispatch={dispatch} />
       </div>
     </>
