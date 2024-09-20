6098df9876de90cd34070596d8bc5d39fa40bf12
Xiahui Zhang <xhz0729@gmail.com>
2024-09-19 20:06:39 -0500

    adding link to detail page

--- animal-sighting-tracker/client/src/components/SightingCard.jsx
+++ animal-sighting-tracker/client/src/components/SightingCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const SightingCard = ({sighting, dispatch}) => {
-
   return (
     <div>
         <h3>Nickname: {sighting.nickname}</h3>
         <p>Sighting Location: {sighting.location}</p>
         <p>Sighting Date: {new Date(sighting.sighting_date).toLocaleDateString()}</p>
+
+
+        {/* Link to the individual detail page */}
+        <Link to={`/individuals/${sighting.individual_id}`}>
+          View Details
+        </Link>
     </div>
   )
 }
