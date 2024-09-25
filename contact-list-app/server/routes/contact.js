a126e73882a9c9a87d59cf2ee1c60ab7c21e0094
Xiahui Zhang <xhz0729@gmail.com>
2024-09-25 00:15:08 -0500

    adding routes to delete, add, update, show contact details

--- contact-list-app/server/routes/contact.js
+++ contact-list-app/server/routes/contact.js
@@ -1,8 +1,27 @@
 import express from "express";
-import { getContact } from "../controller/contact.js";
+import {
+  getContact,
+  getContactDetails,
+  addContact,
+  deleteContactById,
+  changeContactById,
+} from "../controller/contact.js";
 
 const router = express.Router();
-// get all species
+
+// get all contacts
 router.get("/", getContact);
+
+// get contact's details
+router.get("/:id", getContactDetails);
+
+// add a new contact
+router.post("/", addContact);
+
+// delete a contact
+router.delete("/:id", deleteContactById);
+
+//update a contact
+router.put("/:id", changeContactById);
 
 export default router;
