d16dd9ace5461d7cc63a9c46cc89470fae6e2659
Xiahui Zhang <xhz0729@gmail.com>
2024-09-25 23:53:33 -0500

    adding route to add or update a user

--- weather-app/server/index.js
+++ weather-app/server/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import cors from "cors";
 import db from "./db/db-connection.js";
 
 dotenv.config();
 
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json()); // <--- This line is crucial
+
+app.use(cors());
 
 //Set the port that you want the server to run on
 const PORT = process.env.PORT || 8080;
@@ -41,6 +47,48 @@ app.get("/api/weather", async (req, res) => {
     res.json(data);
   } catch (error) {
     console.log(error);
+  }
+});
+
+// route to update or insert a new user record
+app.post("/user", async (req, res) => {
+  let { username, favorite_city, user_email } = req.body;
+
+  try {
+    // Check if the user already exists
+    const user = await db.query("SELECT * FROM users WHERE user_email = $1", [
+      user_email,
+    ]);
+
+    if (user.rows.length > 0) {
+      // If user exists, update the favorite city
+      const updatedQuery = `UPDATE users  SET username = $1, favorite_city = $2  WHERE user_email = $3 RETURNING *`;
+      const updatedUser = await db.query(updatedQuery, [
+        username,
+        favorite_city,
+        user_email,
+      ]);
+      // Send updated user data back
+      res.status(200).json(updatedUser.rows[0]);
+    } else {
+      // Add a new user record if email not found
+      const insertQuery = `
+        INSERT INTO users (username, user_email, favorite_city)
+        VALUES ($1, $2, $3)
+        RETURNING *`;
+      const newUser = await db.query(insertQuery, [
+        username,
+        user_email,
+        favorite_city,
+      ]);
+
+      // Send newly created user data back
+      res.status(201).json(newUser.rows[0]);
+    }
+  } catch (error) {
+    // Catch any errors and respond with an error message
+    console.error(error.message);
+    res.status(500).json({ error: "Error adding/updating a new contact" });
   }
 });
 
