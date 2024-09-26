7676e50223fda296a4b07a2e9dfc1a678f2f277d
Xiahui Zhang <xhz0729@gmail.com>
2024-09-25 21:21:26 -0500

    configure my backend and database connection

--- weather-app/server/db/db-connection.js
+++ weather-app/server/db/db-connection.js
@@ -1,11 +1,9 @@
 import dotenv from "dotenv";
 import pg from "pg";
-
 const { Pool } = pg;
 dotenv.config();
 
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URI,
 });
-
 export default db;
