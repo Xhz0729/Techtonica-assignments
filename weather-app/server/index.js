467c9e1ce3454277d9d4ed167df4660c724473fa
Xiahui Zhang <xhz0729@gmail.com>
2024-09-27 11:58:16 -0500

    update my backend error handling

--- weather-app/server/index.js
+++ weather-app/server/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
 import cors from "cors";
@@ -16,41 +16,48 @@ app.use(cors());
 //Set the port that you want the server to run on
 const PORT = process.env.PORT || 8080;
 
-// get apiKey from .env file
+// Get apiKey from .env file
 const apiKey = process.env.API_KEY;
 
-//creates an endpoint for the route /api
+// Creates an endpoint for the route /api
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from ExpressJS" });
 });
 
-// creates an endpoint for the route /api/weather
+// Creates an endpoint for the route /api/weather
 app.get("/api/weather", async (req, res) => {
   const city = req.query.city;
 
-  // built-in API request by city name
+  // Built-in API request by city name
   // &units=imperial to make temperature shows in fahrenheit
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-  // deal with city is empty
+  // Check if city is empty
   if (!city) {
     return res.status(400).json({ message: "City name is required" });
   }
 
-  // fetch data from API
+  // Fetch data from API
   try {
     const response = await fetch(url);
     const data = await response.json();
     if (!response.ok) {
-      return res.status(response.status).json({ message: data.message });
+      return res.status(response.status).json({
+        message:
+          data.message ||
+          "There was an issue fetching the weather data. Please try again later.",
+      });
     }
     res.json(data);
   } catch (error) {
     console.log(error);
+    return res.status(response.status).json({
+      message: "We’re experiencing server issues. Please try again later.",
+    });
   }
 });
 
-// route to update or insert a new user record
+// Route to update or insert a new user record
 app.post("/user", async (req, res) => {
   let { username, favorite_city, user_email } = req.body;
 
@@ -60,12 +67,13 @@ app.post("/user", async (req, res) => {
       user_email,
     ]);
 
+    // User exist
     if (user.rows.length > 0) {
       // Only update the favorite city if it is not null (i.e., user checked the box)
       const currentFavoriteCity = favorite_city || user.rows[0].favorite_city;
 
-      // If user exists, update the favorite city
-      const updatedQuery = `UPDATE users  SET username = $1, favorite_city = $2  WHERE user_email = $3 RETURNING *`;
+      // If user exists, update the favorite city and username accordingly
+      const updatedQuery = `UPDATE users SET username = $1, favorite_city = $2 WHERE user_email = $3 RETURNING *`;
       const updatedUser = await db.query(updatedQuery, [
         username,
         currentFavoriteCity,
