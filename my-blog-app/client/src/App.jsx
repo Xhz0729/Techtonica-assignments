18c7838338449e46d3d9752fc3a96c10480d29dc
Xiahui Zhang <xhz0729@gmail.com>
2024-10-03 00:10:36 -0700

    render components

--- my-blog-app/client/src/App.jsx
+++ my-blog-app/client/src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from './components/Login'
 import Register from './components/Register'
+import HomePage from './components/HomePage';
+import Posts from './components/Posts';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
-        <Login />
-        <Register />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
